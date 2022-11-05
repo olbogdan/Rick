0b6b3eb745b28de36bb2bfdf1f42a4b10266e19d
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-11-05 23:01:40 +0100

    feat detail _n_ list pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'character',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'character',
+    loadChildren: () => import('./pages/character/list/list.module').then( m => m.ListPageModule)
+  },
+  {
+    path: 'character/:id',
+    loadChildren: () => import('./pages/character/details/details.module').then( m => m.DetailsPageModule)
   },
 ];
 
