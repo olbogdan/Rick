79c3b15a6ecef8b9e243b81ec70040853c122298
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-11-13 22:18:28 +0100

    add in-app-browser lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx'
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy}, InAppBrowser],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
