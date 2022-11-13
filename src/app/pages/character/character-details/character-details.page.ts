61893bb99d5836a9bcd0f2fdf60a28872e1c24d0
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-11-13 22:19:43 +0100

    open episode in external browser

--- src/app/pages/character/character-details/character-details.page.ts
+++ src/app/pages/character/character-details/character-details.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { RickAndMortyCharacter } from 'src/app/models/character.model'
 import { RickApiService } from 'src/app/services/rick-api.service'
 import { ActivatedRoute } from '@angular/router'
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx'
 
 @Component({
   selector: 'app-character-details',
@@ -12,12 +13,20 @@ export class CharacterDetailsPage implements OnInit {
   character: RickAndMortyCharacter
   constructor(
     private activated: ActivatedRoute,
-    private rickAndMortyService: RickApiService
+    private rickAndMortyService: RickApiService,
+    private iab: InAppBrowser
   ) { }
 
   async ngOnInit() {
     const id = parseInt(this.activated.snapshot.paramMap.get('id') ?? '0', 10)
     this.character = await this.rickAndMortyService.getCharacter(id)
     console.log(this.character)
+  }
+
+  //planed to use for opening episodes
+  launch(url: string) {
+    console.log(url)
+    const browser = this.iab.create(url)
+    browser.show()
   }
 }
