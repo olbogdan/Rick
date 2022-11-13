5deb71cc1c9bb32b6659bfacffd5333f997c78ce
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-11-13 20:47:57 +0100

    implemented rick api service

--- src/app/services/rick-api.service.ts
+++ src/app/services/rick-api.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { CHARACTERS_API } from '../constants/urls'
+import { environment } from '../../environments/environment'
+import { RickAndMortyCharactersResponse } from '../models/charactersResponse.model'
+import { RickAndMortyCharacter } from '../models/character.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class RickApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  async getCharacters(
+    name: string = '',
+    page: number = 1
+  ): Promise<RickAndMortyCharactersResponse> {
+    return this.http.get<RickAndMortyCharactersResponse>(
+        `${environment.apiUrl}/${CHARACTERS_API}?name=${name}&page=${page}`
+      ).toPromise()
+  }
+
+  async getCharacter(id: number): Promise<RickAndMortyCharacter> {
+    return this.http.get<RickAndMortyCharacter>(
+        `${environment.apiUrl}/${CHARACTERS_API}/${id}`
+      ).toPromise()
+  }
 }
